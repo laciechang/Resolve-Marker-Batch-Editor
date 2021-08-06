fc686df56a2b41c4938ca096cda7439769212793
laciechang <laciechang@163.com>
2021-08-07 00:36:27 +0800

    ov

---
+++ Marker_Batch_Editor_v1.py
@@ -0,0 +1,191 @@
+# -*- coding:utf-8 -*-
+# Author: 张来吃
+# Version: 1.0.0
+# Contact: laciechang@163.com
+
+# -----------------------------------------------------
+# This script must be run in DaVinci Resolve only.
+# -----------------------------------------------------
+
+
+import re, math
+
+marker_color = ['Blue','Cyan','Green','Yellow','Red','Pink','Purple','Fuchsia','Rose','Lavender','Sky','Mint','Lemon','Sand','Cocoa','Cream']
+
+def _exit(ev):
+    disp.ExitLoop()
+
+def getresolve(app='Resolve'):
+    dr = bmd.scriptapp(app)
+    return dr
+
+def this_pj():
+    pj_manager = getresolve().GetProjectManager()
+    current_pj = pj_manager.GetCurrentProject()
+    return current_pj
+
+def this_timeline():
+    return this_pj().GetCurrentTimeline()
+
+def frame_to_index(i):
+    lenth = len(str(max(read_all_marker().keys())))
+    b = int(math.pow(10, int(lenth)))
+    o = str(b+int(i))[1:]
+    return o
+
+def read_all_marker():
+    mks = this_timeline().GetMarkers()
+    return mks
+
+def add_markers(frameId, color, name, note, duration, customData=''):
+    o = this_timeline().AddMarker(frameId, color, name, note, duration, customData)
+    return o
+
+def del_markers(by, frameNum, color):
+    if by is 'frame':
+        o = this_timeline().DeleteMarkerAtFrame(frameNum)
+    elif by is 'color':
+        o = this_timeline().DeleteMarkersByColor(color)
+    else:
+        o = False
+    return o
+
+def edit_marker(type, before, after):
+    pattern = re.compile(str(before))
+    all_marker = read_all_marker()
+    for mk_frameId in all_marker:
+        mk = all_marker[mk_frameId]
+        color = str(mk['color'])
+        duration = int(mk['duration'])
+        note = str(mk['note'])
+        name = str(mk['name'])
+        customData = mk['customData']
+        if type is 'color':
+            if before==color:
+                del_markers('frame', mk_frameId, color)
+                add_markers(mk_frameId, after, name, note, duration, customData)
+            else:
+                pass
+        elif type is 'name':
+            if re.search(pattern, name) is not None:
+                new_name = re.sub(pattern, after, name)
+                del_markers('frame', mk_frameId, color)
+                add_markers(mk_frameId, color, new_name, note, duration, customData)
+            else:
+                pass
+        elif type is 'note':
+            if re.search(pattern, note) is not None:
+                new_note = re.sub(pattern, after, note)
+                del_markers('frame', mk_frameId, color)
+                add_markers(mk_frameId, color, name, new_note, duration, customData)
+            else:
+                pass
+        else:
+            pass
+        print(color, duration, note, name, customData)
+
+def _edit_color(ev):
+    before = itm['color_a'].CurrentText
+    after = itm['color_b'].CurrentText
+    if before==after:
+        pass
+    else:
+        itm['edit_color'].Enabled = False
+        itm['edit_color'].Text = '喝口茶'
+        edit_marker('color', before, after)
+        itm['edit_color'].Enabled = True
+        itm['edit_color'].Text = '修改'
+    
+
+def _edit_notes(ev):
+    before = itm['notes_a'].Text
+    after = itm['notes_b'].Text
+    if before==after:
+        pass
+    else:
+        itm['edit_notes'].Enabled = False
+        itm['edit_notes'].Text = '喝口茶'
+        edit_marker('note', before, after)
+        itm['edit_notes'].Enabled = True
+        itm['edit_notes'].Text = '修改'
+    
+
+def _edit_name(ev):
+    before = itm['name_a'].Text
+    after = itm['name_b'].Text
+    if before==after:
+        pass
+    else:
+        itm['edit_name'].Enabled = False
+        itm['edit_name'].Text = '喝口茶'
+        edit_marker('name', before, after)
+        itm['edit_name'].Enabled = True
+        itm['edit_name'].Text = '修改'
+
+def _del_by_color(ev):
+    targ = itm['color_a'].CurrentText
+    del_markers('color', '', targ)
+
+def main_ui(ui):
+    window = ui.VGroup({"Spacing": 10,},[
+        
+        
+        ui.HGroup({"Spacing": 10, "Weight": 0,},[ 
+            ui.Label({ "ID": "color","Text": "Color"}),
+            ui.ComboBox({ "ID": "color_a","Weight": 4}),
+            ui.ComboBox({ "ID": "color_b","Weight": 4}),
+            ui.Button({ "ID": "edit_color", "Text": "替换","Weight": 0}),
+        ]),
+        ui.HGroup({"Spacing": 10, "Weight": 0,},[
+            ui.HGap(),
+            ui.Button({ "ID": "del_by_color", "Text": "删除","Weight": 0}),
+        ]),
+        ui.HGroup({"Spacing": 10, "Weight": 0,},[ 
+            ui.Label({ "ID": "notes","Text": "Notes"}),
+            ui.LineEdit({ "ID": "notes_a","Weight": 4}),
+            ui.LineEdit({ "ID": "notes_b","Weight": 4}),
+            ui.Button({ "ID": "edit_notes", "Text": "修改","Weight": 0}),
+        ]),
+        ui.HGroup({"Spacing": 10, "Weight": 0,},[ 
+            ui.Label({ "ID": "name","Text": "Name"}),
+            ui.LineEdit({ "ID": "name_a","Weight": 4}),
+            ui.LineEdit({ "ID": "name_b","Weight": 4}),
+            ui.Button({ "ID": "edit_name", "Text": "修改","Weight": 0}),
+        ]),
+        ])
+    return window
+
+
+if __name__ == '__main__':
+    fu = bmd.scriptapp('Fusion')
+
+    ui = fu.UIManager
+    disp = bmd.UIDispatcher(ui)
+
+    window_01 = main_ui(ui)
+
+    dlg = disp.AddWindow({ 
+                        "WindowTitle": "Marker Editor", 
+                        "ID": "MyWin", 
+                        "Geometry": [ 
+                                    600, 600, 
+                                    600, 160
+                         ], 
+                        },
+    window_01)
+
+    itm = dlg.GetItems()
+
+    itm['color_a'].AddItems(marker_color)
+    itm['color_b'].AddItems(marker_color)
+
+    dlg.On.edit_color.Clicked = _edit_color
+    dlg.On.edit_name.Clicked = _edit_name
+    dlg.On.edit_notes.Clicked = _edit_notes
+    dlg.On.del_by_color.Clicked = _del_by_color
+    
+
+    dlg.On.MyWin.Close = _exit
+    dlg.Show()
+    disp.RunLoop()
+    dlg.Hide()
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Resolve-Marker-Batch-Editor
-Batch edit timeline marker in DaVinci Resolve 17.
+## 🔧安装
+
+- 请将 *Marker_Batch_Editor_v1.py* 拷贝至达芬奇指定的脚本存放目录下
+
+- macOS: /Users/{你的用户名}/Library/Application Support/Blackmagic Design/DaVinci Resolve/Fusion/Scripts/Edit
+
+- Windows: C:\Users\{你的用户名}\AppData\Roaming\Blackmagic Design\DaVinci Resolve\Support\Fusion\Scripts\Edit
+
+- 使用 v17.2 及以上版本 (老版本似乎不支持中文噢)
+
+- 在菜单：工作区(Workspace) > 脚本(Scripts) 中即可找到
+
+- 仅支持在达芬奇内使用，不支持在外部运行
+
+  
+
+## 🎛用法
+
+- 在工具中提供了 *标记颜色、标记名称、标记备注的替换*，以及 *按标记颜色删除* 功能
+
+- 用法类似绝大多数系统自带的“查找/替换”功能，即左侧输入框为“查找”，右侧为“替换”
+- 支持 正则语法*
+
+
+
+# 🧷TIPS 
+
+要删除指定内容，右侧目标栏留空即可
+
+
+
+## ☝️需要
+
+- Python 3.6 64-bit 
+- 其他版本的 Python 达芬奇并不支持
